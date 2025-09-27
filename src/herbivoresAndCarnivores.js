f9c154277b22f6fe996aa8883faf698a7bf0c48b
Roman <hpp1ovh@gmail.com>
2025-09-27 12:12:33 +0300

    complit task

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -2,14 +2,47 @@
 
 class Animal {
   // write your code here
+
+  static alive = [];
+
+  health = 100;
+  constructor(name) {
+    this.name = name;
+    Animal.alive.push(this);
+  }
 }
 
 class Herbivore extends Animal {
   // write your code here
+  hidden = false;
+
+  hide() {
+    this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
   // write your code here
+
+  bite(beast) {
+    if (
+      beast instanceof Herbivore &&
+      beast.hidden === false &&
+      beast.health > 0
+    ) {
+      beast.health -= 50;
+
+      if (beast.health <= 0) {
+        beast.health = 0;
+
+        const index = Animal.alive.indexOf(beast);
+
+        if (index !== -1) {
+          Animal.alive.splice(index, 1);
+        }
+      }
+    }
+  }
 }
 
 module.exports = {
