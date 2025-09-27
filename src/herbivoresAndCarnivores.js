76fa6e0fc7002b56cbd9c1135799eb8d59eb9987
Roman <hpp1ovh@gmail.com>
2025-09-27 13:34:07 +0300

    successfully complite

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,32 +1,37 @@
 'use strict';
 
 class Animal {
-  // write your code here
-
   static alive = [];
 
-  health = 100;
   constructor(name) {
     this.name = name;
+    this.health = 100;
+
+    this.record = { name: this.name, health: this.health, hidden: false };
+
     Animal.alive.push(this);
   }
 }
 
 class Herbivore extends Animal {
-  // write your code here
   hidden = false;
+
+  constructor(name) {
+    super(name);
+    this.record.hidden = this.hidden;
+  }
 
   hide() {
     this.hidden = true;
+    this.record.hidden = true;
   }
 }
 
 class Carnivore extends Animal {
-  // write your code here
-
   bite(beast) {
-    if (beast instanceof Herbivore && !beast.hidden && beast.health > 0) {
+    if (beast instanceof Herbivore && !beast.hidden) {
       beast.health -= 50;
+      beast.record.health = beast.health;
 
       Animal.alive = Animal.alive.filter((a) => a.health > 0);
     }
